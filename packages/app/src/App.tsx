898355b98b60425f772b64b8669b5d3a660e1bdb
Assaf Koren <assafnoahkoren@gmail.com>
2025-05-09 19:38:21 +0300

    .

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { setupIonicReact } from '@ionic/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
+setupIonicReact({
+  mode: 'ios',
+});
+
 root.render(
   <React.StrictMode>
     <App />
